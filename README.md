5d0ff7f55ea46991219576a08bd053035e69eb7f
clement <clement.demonchy@jobteaser.com>
2019-06-23 16:33:27 +0200

    This finally work

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # itm
+
 Image to mongodb
+
+# Purpose 
+
+Developper un workflow asynchrone de distribution de taches en python3 sous docker.
+
+Objectifs:
+- Recuperer les images accessibles par les urls contenues dans le fichier urls.txt.
+- Developper une tache qui calculera la MD5 de l'image
+- Developper une tache qui transformera l'image en niveau de gris avec le calcul suivant: (R + G + B) / 3
+- Developper une tache qui recuperera les outputs des deux precedents workers. Il devra egalement inserer dans une base
+de donnees MongoDB, les informations (md5, l'image en niveau de gris, la hauteur et la largeur de l'image, la date d'insertion).
+- Developper une api en Flask pour visualiser les images stockees dans MongoDB via l'url http://localhost:5000/image/<MD5>
+- Developper une api en Flask de monitoring via l'url http://localhost:5000/monitoring. Cette api devra retourner un
+histogramme decrivant le nombre d'images traitees avec succes ou erreur par interval d'une minute. (Prevoir une collection dans MongoDB pour recuperer ces informations)
+
+
+Notes:
+- Certaines URLs retournent une erreur
+- Il ne doit pas y avoir de doublons d'image dans la base MongoDB (unicite du MD5)
+- Les technologies autres que celles citees sont libres
+- Le travail sera rendu sur un repo git public
---
+++ backend/download.py
@@ -0,0 +1,44 @@
+import typing
+import uuid
+#request don't work in docker for this website https://github.com/kennethreitz/requests/issues/3948
+#import urllib3
+import urllib.request
+
+def download_url(url: str, save_directory: str, error_directory: str
+) -> typing.Tuple[bool, str]:
+    url_uuid = uuid.uuid1()
+    download_success = False
+    try:
+        print(url)
+        #r = http.request('GET',url.replace("https","http"))
+        r= urllib.request.urlopen(url)
+        if r.getcode() == 200:
+            download_success = True
+    except Exception as e:
+        print(e)
+    if download_success:
+        filepath = f"{save_directory}/{url_uuid}"
+        print(r.read())
+        open(filepath, "wb").write(r.read())
+    else:
+        filepath = f"{error_directory}/{url_uuid}"
+        open(filepath, "w").write("")
+    return download_success, filepath
+
+def parse_url_file(url_filepath: str):
+    count = 0
+    with open(url_filepath, "r") as url_file:
+        for line in url_file:
+            success, result_filepath = download_url(
+                url=line,
+                save_directory="data/download",
+                error_directory="data/download_error",
+            )
+            print(result_filepath)
+            count += 1
+            if count == 10:
+                break
+
+
+if __name__ == "__main__":
+    parse_url_file(url_filepath="urls.txt")
