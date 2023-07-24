4bd9c1a944a672bad690f83da54d533f4b1bfef6
knil-sama <cols.knil@gmail.com>
2023-07-24 14:54:03 +0000

    feat(api & dev): done with testing fastapi part and added devcontainer setup for vscode

--- api/api/application.py
+++ api/api/application.py
@@ -1,6 +1,7 @@
 # from pydantic import BaseModel
 from typing import Any
 
+import pydantic
 from fastapi import FastAPI, HTTPException, Response
 
 from api.database import get_image, get_images, get_monitoring
@@ -8,19 +9,25 @@ from api.database import get_image, get_images, get_monitoring
 app = FastAPI()
 
 
+class MonitoringReponse(pydantic.BaseModel):
+    execution_date: str
+    success: pydantic.types.NonNegativeInt
+    error: pydantic.types.NonNegativeInt
+
+
 @app.get("/")
 def read_root() -> dict[str, str]:
     return {"message": "Hello, this api exist to query data, see the docs/"}
 
 
-@app.get("/image/{md5}", responses={200: {"content": {"image/png": {}}}})
-def read_image(md5: str) -> dict:
-    result = get_image(md5)
+@app.get("/image/{image_id}", responses={200: {"content": {"image/png": {}}}})
+def read_image(image_id: str) -> dict:
+    result = get_image(image_id)
     if result is not None:
         # response = make_response(base64.b64decode(result["grayscale"]))
         response = Response(result.grayscale, media_type="image/png")
     else:
-        msg = f"Image {md5} not found"
+        msg = f"Image {image_id} not found"
         raise HTTPException(status_code=404, detail=msg)
     return response
 
@@ -28,14 +35,14 @@ def read_image(md5: str) -> dict:
 @app.get("/images/")
 def read_images() -> list[str]:
     """
-    Return list of all md5 in db
+    Return list of all id in db
     """
     results = get_images()
-    return [result.md5 for result in results]
+    return [result.id for result in results]
 
 
 @app.get("/monitoring/")
-def read_monitoring() -> list[dict[str, Any]] | str:
+def read_monitoring() -> list[MonitoringReponse] | str:
     """
     Returns:
         Stats of succes and error
@@ -44,13 +51,13 @@ def read_monitoring() -> list[dict[str, Any]] | str:
     list_result = get_monitoring()
     if list_result:
         response = [
-            {
-                "execution_date": result["execution_date"].strftime(
+            MonitoringReponse(
+                execution_date=result.execution_date.strftime(
                     "%m/%d/%Y, %H:%M:%S",
                 ),
-                "success": result["success"],
-                "error": result["error"],
-            }
+                success=result.success,
+                error=result.error,
+            )
             for result in list_result
         ]
     return response
--- api/api/database.py
+++ api/api/database.py
@@ -8,27 +8,30 @@ from models.monitoring import Monitoring
 MONGO_INITDB_ROOT_USERNAME = os.environ["MONGO_INITDB_ROOT_USERNAME"]
 MONGO_INITDB_ROOT_PASSWORD = os.environ["MONGO_INITDB_ROOT_PASSWORD"]
 
-CLIENT = pymongo.MongoClient(
+MONGO_CLIENT = pymongo.MongoClient(
     "mongodb://mongo:27017",
     username=MONGO_INITDB_ROOT_USERNAME,
     password=MONGO_INITDB_ROOT_PASSWORD,
 )
 
 
-def get_image(md5: str) -> Image | None:
-    mongo_object = CLIENT["image_bank"].images.find_one({"md5": md5})
+def get_image(
+    image_id: str,
+    client: pymongo.MongoClient = MONGO_CLIENT,
+) -> Image | None:
+    mongo_object = client["image_bank"].images.find_one({"id": image_id})
     return Image.parse_object(mongo_object)
 
 
-def get_images() -> list[Image]:
+def get_images(client: pymongo.MongoClient = MONGO_CLIENT) -> list[Image]:
     return [
         Image.parse_object(mongo_object)
-        for mongo_object in CLIENT["image_bank"].images.find({}, {"md5": 1})
+        for mongo_object in client["image_bank"].images.find({}, {"id": 1})
     ]
 
 
-def get_monitoring() -> list[Monitoring]:
+def get_monitoring(client: pymongo.MongoClient = MONGO_CLIENT) -> list[Monitoring]:
     return [
         Monitoring.parse_object(mongo_object)
-        for mongo_object in CLIENT["image_bank"].monitoring.find({})
+        for mongo_object in client["image_bank"].monitoring.find({})
     ]
